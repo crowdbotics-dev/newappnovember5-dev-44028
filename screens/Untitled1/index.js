5e28b8529acd6c09376d1889ee9c7b310c54a44a
Aline Tavares <aline@crowdbotics.com>
2022-11-11 12:26:01 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,9 +1,13 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled1 = () => {
+const Untitled1 = ({
+  navigation
+}) => {
   return <View style={_styles.nejIoQHl}>
-      </View>;
+      <Pressable onPress={() => navigation.navigate("documents")} style={_styles.yKXfEJAA}><Text style={_styles.vaDzZmKp}>Click to Navigate To Documents</Text></Pressable></View>;
 };
 
 export default Untitled1;
@@ -14,5 +18,20 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  vaDzZmKp: {
+    left: 82,
+    top: 403,
+    position: "absolute",
+    width: 207,
+    height: 115,
+    lineHeight: 24,
+    fontSize: 24,
+    borderRadius: 0,
+    fontFamily: "Roboto",
+    fontWeight: "700"
+  },
+  yKXfEJAA: {
+    position: "unset"
   }
 });
