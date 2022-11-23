0990f5c6141839eaa65d7adda434aec9dd0505c0
Aline Tavares <aline@crowdbotics.com>
2022-11-23 14:58:53 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,9 @@
+const styles = StyleSheet.create({});
 export const globalOptions = {
-  name: "newappnovember5-dev-44028",
-  url: "https://newappnovember5-dev-44028.botics.co",
-  api: "https://newappnovember5-dev-44028.botics.co/api/v1"
+  "name": "newappnovember5-dev-44028",
+  "url": "https://newappnovember5-dev-44028.botics.co",
+  "api": "https://newappnovember5-dev-44028.botics.co/api/v1"
 };
 export const modulesOptions = {
-  "@modules/app-menu": {
-    copy: "Routes available!"
-  }
+  "@modules/app-menu": {}
 };
