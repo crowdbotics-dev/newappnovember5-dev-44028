bc31d8c7846fdf7827fd79de2ce615632f059865
Aline Tavares <aline@crowdbotics.com>
2022-11-23 15:17:10 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,9 +1,18 @@
+import { RadioGroup } from "react-native-radio-buttons-group";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
   return <View style={_styles.UwSbYhXx}>
-      </View>;
+      <RadioGroup style={_styles.lxYCFCgz} radioButtons={[{
+      id: "1",
+      label: "Option 1",
+      value: "option1"
+    }, {
+      id: "2",
+      label: "Option 2",
+      value: "option2"
+    }]} layout="column"></RadioGroup></View>;
 };
 
 export default Untitled1;
@@ -14,5 +23,9 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  lxYCFCgz: {
+    width: 120,
+    height: 70
   }
 });
